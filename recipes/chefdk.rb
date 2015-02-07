d4f1d7dd6012315772ae7e4b3642a000a42fbe4c
Giovanni Toraldo <me@gionn.net>
2015-02-07 19:08:23 +0100

    Avoid installing chefdk and vagrant if they are present

--- recipes/chefdk.rb
+++ recipes/chefdk.rb
@@ -3,6 +3,7 @@ remote_file "#{Chef::Config[:file_cache_path]}/chefdk_0.4.0-1_amd64.deb" do
   source "https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.4.0-1_amd64.deb"
   mode '0644'
   notifies :run, "execute[install-chefdk]", :immediately
+  not_if "dpkg -l | grep chefdk | grep 0.4.0-1"
 end
 
 execute "install-chefdk" do
--- recipes/vagrant.rb
+++ recipes/vagrant.rb
@@ -3,6 +3,7 @@ remote_file "#{Chef::Config[:file_cache_path]}/vagrant_1.7.2_x86_64.deb" do
   source "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb"
   mode '0644'
   notifies :run, "execute[install-vagrant]", :immediately
+  not_if "dpkg -l | grep vagrant | grep 1.7.2"
 end
 
 execute "install-vagrant" do
