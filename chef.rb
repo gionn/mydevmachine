b4a1088de81231881472b3a590ebe411cc304817
Giovanni Toraldo <me@gionn.net>
2015-06-14 17:01:07 +0200

    Added support to import node.json for the current machine

--- chef.rb
+++ chef.rb
@@ -1,8 +1,11 @@
+require 'socket'
+
 cwd = File.expand_path(File.dirname(__FILE__))
 home = File.expand_path('~')
 
 data_bag_path cwd + "/data_bags"
 cookbook_path cwd + "/cookbooks"
 environment_path cwd + "/environment"
+json_attribs cwd + '/nodes/' + Socket.gethostname + '.json'
 environment "default"
 cache_path "#{home}/.local/share/chef-client-cache"
--- converge.sh
+++ converge.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-berks vendor cookbooks && sudo chef-client -z -o 'mydevmachine' -c chef.rb
+berks vendor cookbooks && sudo chef-client -z -c chef.rb
