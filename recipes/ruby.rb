6897063aa7560f4c7c9f662b64ebb5fc99a30cb0
Giovanni Toraldo <me@gionn.net>
2015-06-14 20:54:38 +0200

    Adopted suggested build env for rbenv, moved version selection to users data bag

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -1,8 +1,18 @@
 
 include_recipe "ruby_build"
 
-package "libffi-dev"
+# https://github.com/sstephenson/ruby-build/wiki#suggested-build-environment
+%w(autoconf bison build-essential).each do |build_pkg|
+  package build_pkg
+end
 
-ruby_build_ruby "2.2.0"
+%w(libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev).each do |dep_pkg|
+  package dep_pkg
+end
+
+search(:users, "*:*").each do |user|
+  user['rubies'].each do |version|
+    ruby_build_ruby version
+end
 
 include_recipe "rbenv::user"
