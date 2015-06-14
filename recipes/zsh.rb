bccd3368f0e5d6511dce3416d7d028b6279333ee
Giovanni Toraldo <me@gionn.net>
2015-06-14 17:10:11 +0200

    Populate .zprofile from users data bag.

--- recipes/zsh.rb
+++ recipes/zsh.rb
@@ -1,3 +1,13 @@
 
 include_recipe 'prezto'
 package 'byobu'
+
+search(:users, "*:*").each do |user|
+  file "/home/#{user['id']}/.zprofile" do
+    content user['zprofile'].join("\n")
+    mode '0775'
+    owner user['id']
+    group user['gid']
+    action :create
+  end
+end
