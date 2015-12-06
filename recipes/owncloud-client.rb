27bcb626c011dc1c3ed7c490f077a707c0862b25
Giovanni Toraldo <me@gionn.net>
2015-12-06 12:06:15 +0100

    Added variable to owncloud keyring url

--- recipes/owncloud-client.rb
+++ recipes/owncloud-client.rb
@@ -1,8 +1,8 @@
 
-apt_repository 'owncloud-client' do
+apt_repository "owncloud-client" do
   uri          "http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_#{node['lsb']['release']}/"
-  distribution '/'
-  key          'http://download.opensuse.org/repositories/isv:ownCloud:desktop/xUbuntu_15.04/Release.key'
+  distribution "/"
+  key          "http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_#{node['lsb']['release']}/Release.key"
 end
 
 package 'owncloud-client'
