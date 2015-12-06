1c6d4cdec021bc6f1fc1cd68c4242045d760b2f0
Giovanni Toraldo <me@gionn.net>
2015-12-06 12:05:28 +0100

    Moved chef-dk handling in converge.sh script

--- converge.sh
+++ converge.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-berks vendor cookbooks && sudo chef-client -z -c chef.rb
+if ! chef-client -v; then
+  curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -c current -P chefdk
+fi
+
+berks vendor cookbooks
+sudo chef-client -z -c chef.rb
