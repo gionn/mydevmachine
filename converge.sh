140b048b3838ea04349df3dfb443e19f8d656012
Giovanni Toraldo <me@gionn.net>
2015-12-06 22:07:04 +0100

    Ensure curl is present in converge.sh script

--- converge.sh
+++ converge.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-if ! chef-client -v; then
+if ! chef-client -v > /dev/null; then
+  sudo apt-get update
+  sudo apt-get install -y curl
   curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -c current -P chefdk
 fi
 
+rm -fr cookbooks
 berks vendor cookbooks
 sudo chef-client -z -c chef.rb
