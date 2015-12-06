34b884ade11c3fc86dee8650c1401318fb912aa7
Giovanni Toraldo <me@gionn.net>
2015-12-06 22:26:29 +0100

    Configures zshrc instead of zprofile

--- recipes/zsh.rb
+++ recipes/zsh.rb
@@ -2,11 +2,14 @@
 include_recipe 'prezto'
 
 search(:users, "*:*").each do |user|
-  file "/home/#{user['id']}/.zprofile" do
-    content user['zprofile'].join("\n")
+  template "/home/#{user['id']}/.zshrc" do
     mode '0775'
     owner user['id']
     group user['gid']
+    variables({
+      'rows' => user['zshrc']
+    })
+    source 'zshrc.erb'
     action :create
   end
 end
