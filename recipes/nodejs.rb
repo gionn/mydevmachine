f05fbe3f407b957c17807cd953fa69e55e078ad7
Giovanni Toraldo <me@gionn.net>
2015-12-06 12:10:19 +0100

    Switched to nvm for nodejs

--- recipes/nodejs.rb
+++ recipes/nodejs.rb
@@ -1,2 +1,13 @@
-node.override['nodejs']['install_method'] = 'source'
-include_recipe 'nodejs::nodejs_from_source'
+
+include_recipe 'nvm'
+
+search(:users, "*:*").each do |user|
+  user['nodes'].each do |version|
+    nvm_install version do
+      user user['id']
+      group user['gid']
+      from_source false
+      action :create
+    end
+  end
+end
